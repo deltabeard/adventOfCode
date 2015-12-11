3f30727f6bef184e9d3e993401dfd88c63d4aa0b
Mahyar Koshkouei <deltabeard@users.noreply.github.com>
2015-12-11 22:22:43 +0000

    Day3 - part 1
    Tested working.
    
    Signed-off-by: Mahyar Koshkouei <deltabeard@users.noreply.github.com>

---
+++ day3/main.c
@@ -0,0 +1,83 @@
+/* Day 3: Perfectly Spherical Houses in a Vacuum
+ *
+ * Mahyar Koshkouei
+ */
+
+// lxwxh
+
+#define ARRAY_SIZE 1000
+#define ARRAY_MID (ARRAY_SIZE / 2)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	FILE		*fp;
+	char		char_buffer;
+	static int	houses[ARRAY_SIZE][ARRAY_SIZE];
+	int		x = ARRAY_MID;
+	int		y = ARRAY_MID;
+	int		houses_with_presents = 0;
+
+	fp = fopen("input.txt", "r");
+	if(fp == NULL)
+	{
+		puts("Unable to open file.");
+		return 1;
+	}
+
+	while((char_buffer = fgetc(fp)) != EOF)
+	{
+		/* Delivery at the starting location */
+		houses[x][y]++;
+
+		switch(char_buffer) {
+			case '^' :
+				y++;
+				break;
+			case '>' :
+				x++;
+				break;
+			case 'v' :
+				y--;
+				break;
+			case '<' :
+				x--;
+				break;
+			default :
+				printf("Invalid character \"%c\" found.\n",
+						char_buffer);
+				return 1;
+		}
+
+		/* Delivery at destination location */
+		houses[x][y]++;
+
+		/* Debug: Avoiding seg failts. */
+		if( x == 1 || x == ARRAY_SIZE || y == 1 || y == ARRAY_SIZE)
+		{
+			printf("Approaching array limit. x = %d, y = %d\n",
+					x, y);
+			return 1;
+		}
+	}
+
+	fclose(fp);
+
+	puts("Got here");
+
+	for(int x = 0; x < ARRAY_SIZE; x++)
+	{
+		for(int y = 0; y < ARRAY_SIZE; y++)
+		{
+			if(houses[x][y] > 0)
+				houses_with_presents++;
+		}
+	}
+
+	printf("Houses with at least one present: %d\n", houses_with_presents);
+
+	return 0;
+}
